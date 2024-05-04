e19e34bd7d159eec939a6f2109f6e08e378d4896
CesarrAnn <129460134+CesarrAnn@users.noreply.github.com>
2024-05-04 13:08:05 -0500

    feat: update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule} from "@angular/material/toolbar";
+import { MatDialogModule} from "@angular/material/dialog";
+import {HttpClientModule} from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarContentComponent } from './wanderlog/components/toolbar-content/toolbar-content.component';
+import {MatButton} from "@angular/material/button";
+import { BodyContentComponent } from './wanderlog/components/body-content/body-content.component';
+import {NgOptimizedImage} from "@angular/common";
+import { PopularPlacesDialogComponent } from './wanderlog/components/popular-places-dialog/popular-places-dialog.component';
+import { SpecialOffersDialogComponent } from './wanderlog/components/special-offers-dialog/special-offers-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarContentComponent,
+    BodyContentComponent,
+    PopularPlacesDialogComponent,
+    SpecialOffersDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButton,
+    NgOptimizedImage,
+    MatDialogModule,
+    HttpClientModule
   ],
   providers: [
     provideClientHydration()
